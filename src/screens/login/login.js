d8134e2a40dc0dae915fa65a6cb647d5324fa4da
Sergio Andres Florez <sergio.florez@grability.com>
2020-04-08 16:31:48 -0500

    input component with error and assertive

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Text from '../../components/text/text';
 import Container from '../../components/container/container';
 import Button from '../../components/button/button';
+import Input from '../../components/input/input';
 
 import {useTranslation} from 'react-i18next';
 
@@ -12,6 +13,9 @@ const Login = () => {
       <Text type="headline">{t('login.title')}</Text>
       <Button value="PRIMARY" />
       <Button value="OUTLINE" outlined={true} color="black" />
+      <Input label="Name" />
+      <Input label="Name error" error="Nombre invalido" />
+      <Input label="Password" />
     </Container>
   );
 };
