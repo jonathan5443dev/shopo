2a0235504119807b0750f10adaf61efddeb8d046
Jonathan Buitrago <jonathan.buitrago@grability.com>
2020-04-11 11:38:39 -0500

    analytics

--- src/config/boot-loaders/firebase-loader.js
+++ src/config/boot-loaders/firebase-loader.js
@@ -1,6 +1,7 @@
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import crashlytics from '@react-native-firebase/crashlytics';
+import analytics from '@react-native-firebase/analytics';
 
 export default class FirebaseLoader {
   static boot(App) {
@@ -8,6 +9,7 @@ export default class FirebaseLoader {
       auth: auth(),
       firestore: firestore(),
       crashlytics: crashlytics(),
+      analytics: analytics(),
     };
   }
 }
