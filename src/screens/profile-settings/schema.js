fad2e89ff9a93cada90409c02bbd7fbe375e6919
Sergio Andres Florez <sergio.florez@grability.com>
2020-04-25 13:59:28 -0500

    update profile settings schema

--- src/screens/profile-settings/schema.js
+++ src/screens/profile-settings/schema.js
@@ -4,5 +4,7 @@ import * as yup from 'yup';
 export default yup.object().shape({
   fullname: yup.string().required(i18next.t('login.validations.required')),
   birthdate: yup.string().required(i18next.t('login.validations.required')),
+  oldpassword: yup.string().required(i18next.t('login.validations.required')),
   password: yup.string().required(i18next.t('login.validations.required')),
+  newpassword: yup.string().required(i18next.t('login.validations.required')),
 });
